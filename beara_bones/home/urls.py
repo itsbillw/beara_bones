58fcaa8954ffe251dc52378d8bbef60ada611ac6
itsbillw <itsbillw@gmail.com>
2024-11-22 01:43:32 +0100

    Updating main site styling

--- beara_bones/home/urls.py
+++ beara_bones/home/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import index, poetry
+from .views import index, beara_bones_data, about_me, poetry
 
 app_name = 'home'
 urlpatterns = [
     # Home page.
     path('', index, name='index'),
+    path('data', beara_bones_data, name='data'),
+    path('about', about_me, name='about'),
     path('if-', poetry, name='if-')
 ]
--- beara_bones/home/views.py
+++ beara_bones/home/views.py
@@ -4,5 +4,11 @@ from django.template.response import TemplateResponse
 async def index(request):
     return TemplateResponse(request, "home/index.html")
 
+async def beara_bones_data(request):
+    return TemplateResponse(request, "home/data.html")
+
+async def about_me(request):
+    return TemplateResponse(request, "home/about.html")
+
 async def poetry(request):
     return TemplateResponse(request, "home/if-.html")
